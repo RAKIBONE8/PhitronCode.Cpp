10b9ef16ed97877db9325a3d354de481ba11cdc8
Rakib Mondal <143431829+RAKIBONE8@users.noreply.github.com>
2023-11-14 21:33:35 +0530

    Returning Obj

---
+++ Module 3/Return_Obj.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student
+{
+   public:
+   int roll;
+   int cls;
+   double gpa;
+
+   Student(int r,int c,double g)
+   {
+      roll = r;
+      cls = c;
+      gpa = g;
+   }
+};
+Student* fun()
+{
+    Student rahim(32,10,4.99);
+    Student *p = &rahim;
+    return p;
+}
+
+int main()
+{
+    Student* result = fun();
+    cout<< result->roll<<" "<<result->cls<<" "<<result->gpa<<endl;
+    return 0;
+}
+
+// here is a issue. Because in this case in the function the object is created statically. So after returning the pointer the values are erased.So now need to create the object Dynamically
+// Resolving this in the next file;
