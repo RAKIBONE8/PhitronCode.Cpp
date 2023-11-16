24bab998a051cf0cdff3c7c570ae1cd090948893
Rakib Mondal <143431829+RAKIBONE8@users.noreply.github.com>
2023-11-16 17:54:23 +0530

    Practice Problem 1 & 2

--- Module 3.5/PracticePrblm1.cpp
+++ Module 3.5/PracticePrblm1.cpp
@@ -1,3 +1,16 @@
+/*
+Question: Create three static objects with the help of the constructor of the following class.
+Student
+{
+	name;
+	roll;
+	section;
+	math_marks;
+	cls;
+}
+Then compare those 3 objects and print who got the highest math_marks and print his/her name.
+
+*/
 #include <bits/stdc++.h>
 using namespace std;
 class Student
