e7552059cbe3f95447907afca51725b1794e63fc
Rakib Mondal <143431829+RAKIBONE8@users.noreply.github.com>
2023-11-19 11:46:20 +0530

    Modifiers

---
+++ Module 5/Modifiers.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string a = "hello";
+    // string b = "world";
+    // a += b;
+
+    // a.append(b);
+    // cout << a << endl;
+    // cout << b << endl;
+
+    // a.resize(6);
+    // a[5] = 'A'; -> works
+
+    // a.push_back('A');  works
+    // a.pop_back();
+    string a = "HelloRakib";
+    // a.erase(2,3);
+    // a.replace(2,3," champion ");
+    a.insert(5," champion ");
+    cout<<a<<endl;
+
+    return 0;
+}
