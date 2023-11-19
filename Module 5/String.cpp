f9cdbbec59090e23e8b648ca073461f2a368c5ed
Rakib Mondal <143431829+RAKIBONE8@users.noreply.github.com>
2023-11-19 10:35:05 +0530

    string

---
+++ Module 5/String.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s; // there is no costant size in string(in c++);
+    s = "Rakib\n";
+    // cout<<s;
+    // cout<<s.length();
+    // cout<<s.size(); both function is same;
+    
+    string s1 = "rakib";
+    string s2 = "rakib";
+
+    if(s1 == s2) // here dont have to use strcmp function like c (we directly can compare two string in c++);
+    {
+        cout<<"same string\n";
+    }
+    else
+    {
+        cout<<"not same\n";
+    }
+    return 0;
+}
