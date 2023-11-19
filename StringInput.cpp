e542a1c5dfb69e3cdc7036df4ccba7a44ca001c1
Rakib Mondal <143431829+RAKIBONE8@users.noreply.github.com>
2023-11-19 12:14:11 +0530

    string input

---
+++ StringInput.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    
+    cin>>x;
+    cin.ignore(); // like get char(in c);
+    string s;
+    getline(cin,s);
+    cout<<x<<endl;
+    cout<<s<<endl;
+    
+    return 0;
+}
