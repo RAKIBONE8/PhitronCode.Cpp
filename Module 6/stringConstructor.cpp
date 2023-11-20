7b3b77a2c3c7e81f6fd62d6dea9878f61b80e05f
Rakib Mondal <143431829+RAKIBONE8@users.noreply.github.com>
2023-11-20 20:16:07 +0530

    string constructor

---
+++ Module 6/stringConstructor.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //1. string s = "Hello world";
+
+    //2. string s("hello world");
+
+    //3. string s("hello rakib",7);
+
+    //4. string a = "hello rakib";
+    // string s(a,4);
+    string s(5,'X');
+    cout<<s<<endl;
+    return 0;
+}
